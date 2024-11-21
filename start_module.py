c9ded843b4afccb6176db14a297aa9ba2260ba48
petteway <spetteway@onevizion.com>
2024-11-21 15:12:20 -0500

    test

--- start_module.py
+++ start_module.py
@@ -2,10 +2,10 @@ import subprocess
 import sys
 import json
 
-installed_dependencies = subprocess.check_output([sys.executable, '-m', 'pip', 'install', '-r', 'python_dependencies.ini']).decode().strip()
-if 'Successfully installed' in installed_dependencies:
-    raise Exception('Some required dependent libraries were installed. ' \
-                    'Module execution has to be terminated now to use installed libraries on the next scheduled launch.')
+#installed_dependencies = subprocess.check_output([sys.executable, '-m', 'pip', 'install', '-r', 'python_dependencies.ini']).decode().strip()
+#if 'Successfully installed' in installed_dependencies:
+#    raise Exception('Some required dependent libraries were installed. ' \
+#                    'Module execution has to be terminated now to use installed libraries on the next scheduled launch.')
 
 from run_module import run_module
 
